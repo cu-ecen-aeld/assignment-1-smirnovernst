decb2a85ce0e80ccf4743ff481f5ac0ac866fa1a
Ernst Smirnov <smirnovernst@gmail.com>
2024-08-02 12:21:30 +0200

    `writer.sh` implementation

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+if [ $# -lt 2 ]
+then 
+    echo "Usage: writer.sh <filename> <string>"
+    exit 1
+fi
+
+WRITEFILE=$1
+WRITESTR=$2
+
+DIRNAME=$(dirname $WRITEFILE)
+if [ ! -d $DIRNAME ]
+then
+    mkdir -p $DIRNAME
+fi
+
+echo $WRITESTR > $WRITEFILE
+if [ $? -ne 0 ]
+then
+    echo "Error: Unable to create file $WRITEFILE"
+    exit 1
+fi
